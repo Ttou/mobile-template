796f898ba8d0b549ef2c18d31f717191716a0d03
wangruijin <1361572192@qq.com>
2022-03-10 18:33:44 +0800

    refactor: 完善类型定义

--- types/jsx.d.ts
+++ types/jsx.d.ts
@@ -2,10 +2,9 @@ declare namespace JSX {
   interface IntrinsicAttributes {
     ['id']?: unknown
     ['class']?: unknown
-    ['style']?: unknown
+    ['style']?: import('vue').StyleValue
     ['v-show']?: unknown
     ['v-model']?: unknown
-    ['v-models']?: unknown
     ['v-slots']?: unknown
 
     [property: string]: unknown
