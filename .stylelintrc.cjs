0bf68721ed0fbd2c282ed928350349c4a48719ce
WangruiJin <1361572192@qq.com>
2023-09-05 10:38:32 +0800

    feat(custom): 调整样式

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -15,7 +15,14 @@ module.exports = defineStylelintConfig({
   rules: {
     'block-no-empty': null,
     'property-no-unknown': [true, { ignoreProperties: ['/composes/'] }],
+    'function-no-unknown': [true, { ignoreFunctions: ['/constant/'] }],
+    'declaration-block-no-redundant-longhand-properties': null,
+    'no-descending-specificity': null,
     'selector-class-pattern': null,
-    'selector-pseudo-class-no-unknown': null
+    'selector-pseudo-class-no-unknown': null,
+    'selector-pseudo-element-no-unknown': [
+      true,
+      { ignorePseudoElements: ['v-deep', 'input-placeholder'] }
+    ]
   }
 })
