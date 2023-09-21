b5b3b52aa97a233dfb311af3cb80d74afd8d9120
wangruijin <1361572192@qq.com>
2023-09-21 11:38:27 +0800

    feat(custom): 增加类型校验命令

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,10 +4,6 @@ declare type ElementRef = Nullable<HTMLElement>
 
 declare type ComponentRef = Nullable<Record<string, any>>
 
-declare type ComponentProps<T> = Partial<
-  import('vue').ExtractPropTypes<ReturnType<T>>
->
-
 declare type Func = (...args: any[]) => any
 
 declare type PromiseFunc = (...args: any[]) => Promise<any>
