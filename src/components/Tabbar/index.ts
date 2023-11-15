c530469d99087583d72de42e383c6c70f6b46bef
wangruijin <1361572192@qq.com>
2023-11-15 11:20:27 +0800

    refactor(custom): 精简代码写法

--- src/components/Tabbar/index.ts
+++ src/components/Tabbar/index.ts
@@ -1,2 +1,2 @@
-export * from './Tabbar.define'
+export { type TabItem } from './Tabbar.define'
 export { default as Tabbar } from './Tabbar.vue'
--- src/components/Welcome/index.ts
+++ src/components/Welcome/index.ts
@@ -1,2 +1,2 @@
-export * from './Welcome.define'
+export { type WelcomeProps, welcomeProps } from './Welcome.define'
 export { default as Welcome } from './Welcome.vue'
